d39ce8f619ae30478952856392a59b1f390055ff
Khanh Khanh <66833938+CromBorw2e36@users.noreply.github.com>
2024-08-29 07:14:40 +0700

    Update dbConnection.js

--- src/common/db/dbConnection.js
+++ src/common/db/dbConnection.js
@@ -1,7 +1,5 @@
 const mysql = require("mysql2");
 const config = require("../../config/config");
-const STRING_QUERY = require("../enum/queryString");
-
 const pool = mysql.createPool(config.db);
 
 const query = (sql, params) => {
