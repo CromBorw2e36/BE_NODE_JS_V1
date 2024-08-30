6eb7d29d4fda89065757568f2507edf4165ac441
Khanh Khanh <66833938+CromBorw2e36@users.noreply.github.com>
2024-08-30 10:04:16 +0700

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,31 @@
 // config.js
 
-const userMail = "pronam730@gmail.com";
-const passwordMail = "pronam730@123";
+const userMail = "admin@truc-anh-bio.io.vn";
+const passwordMail = "trucanhbioiovnadmin";
+
+const dbLocal = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "website_shop",
+};
+
+const dbDemoV1 = {
+  host: "truc-anh-bio.io.vn",
+  user: "tuyendu8_root",
+  password: "qwertyuiop1234567890",
+  database: "tuyendu8_node-js-demo-v1", 
+};
 
 module.exports = {
-  db: {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "website_shop",
-  },
+  db: dbDemoV1,
   emailServer: {
-    service: "gmail", // hoặc bạn có thể sử dụng dịch vụ khác như Outlook, Yahoo, v.v.
+    host: "mail.truc-anh-bio.io.vn", // Máy chủ SMTP của bạn
+    port: 465, // Sử dụng cổng SSL/TLS
+    secure: true, // Kết nối bảo mật SSL/TLS
     auth: {
-      user: userMail, // email của bạn
-      pass: passwordMail, // mật khẩu của email
+      user: userMail, // Địa chỉ email của bạn
+      pass: passwordMail, // Mật khẩu của tài khoản email của bạn
     },
   },
   userMail: userMail,
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,17 @@
 // routers.js
 const express = require("express");
-const UserController = require("../controllers/UserController");
 const API_STRING = require("../common/enum/apiString");
-const LoginController = require("../controllers/loginController");
+const LoginController = require("../controllers/loginController").default;
+const userController = require("../controllers/userController").default;
 
 const router = express.Router();
 
 // Define routes
-router.post(API_STRING.User_Search, UserController.getUsers);
-router.post(API_STRING.User_Insert, UserController.Insert);
-router.post(API_STRING.User_Update, UserController.Update);
-router.post(API_STRING.User_Delete, UserController.Delete);
-router.post(API_STRING.User_Delete_ADMIN, UserController.DeleteEx);
+router.post(API_STRING.User_Search, userController.getUsers);
+router.post(API_STRING.User_Insert, userController.Insert);
+router.post(API_STRING.User_Update, userController.Update);
+router.post(API_STRING.User_Delete, userController.Delete);
+router.post(API_STRING.User_Delete_ADMIN, userController.DeleteEx);
 router.post(API_STRING.LoginServerTypeOne, LoginController.LoginServerTypeOne);
 router.post(API_STRING.LoginServerForgetPassword, LoginController.ForgetPassword);
 router.post(API_STRING.LoginServerUpdatePassword, LoginController.ChangePassword);
